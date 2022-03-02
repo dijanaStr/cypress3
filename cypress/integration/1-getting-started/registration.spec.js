b80aeaef064ad640efd5347d5e09fd6ea84ac8a9
dijanaStr <dijana.strbac93@gmail.com>
2022-03-02 22:09:13 +0100

    domaci

--- cypress/integration/1-getting-started/registration.spec.js
+++ cypress/integration/1-getting-started/registration.spec.js
@@ -6,6 +6,262 @@ describe('registration case', () => {
     it('click register button', () => {
         cy.get('a[href="/register"]').click()
     });
+
+    it('register with email without @', () => {
+        cy.get('input[id="first-name"]').type('Dijana')
+        cy.get('input[id="last-name"]').type('Strbac')
+        cy.get('input[id="email"]').type('dijana.strbac93gmail.com')
+        cy.get('input[id="password"]').type('dijana123')
+        cy.get('input[id="password-confirmation"]').type('dijana123')
+        cy.get('input[type="checkbox"]').click()
+        cy.get('button[type="submit"]').click()
+    });
+   
+
+
+
+
+    it('click register button', () => {
+        cy.visit('/register')
+        cy.get('a[href="/register"]').click()
+    });
+    
+    it('register without credentials', () => {
+        cy.get('input[id="first-name"]').type('')
+        cy.get('input[id="last-name"]').type('')
+        cy.get('input[id="email"]').type('')
+        cy.get('input[id="password"]').type('')
+        cy.get('input[id="password-confirmation"]').type('')
+        cy.get('input[type="checkbox"]').click()
+        cy.get('button[type="submit"]').click()
+    });
+   
+
+
+
+
+    it('click register button', () => {
+        cy.visit('/register')
+        cy.get('a[href="/register"]').click()
+    });
+    
+    it('register with email that missing top level domain', () => {
+        cy.get('input[id="first-name"]').type('Dijana')
+        cy.get('input[id="last-name"]').type('Strbac')
+        cy.get('input[id="email"]').type('dijana.strbac@gmail')
+        cy.get('input[id="password"]').type('dijana123')
+        cy.get('input[id="password-confirmation"]').type('dijana123')
+        cy.get('input[type="checkbox"]').click()
+        cy.get('button[type="submit"]').click()
+    });
+
+
+
+
+
+    it('click register button', () => {
+        cy.visit('/register')
+        cy.get('a[href="/register"]').click()
+    });
+    
+    it('register with wrong password conformation', () => {
+        cy.get('input[id="first-name"]').type('Dijana')
+        cy.get('input[id="last-name"]').type('Strbac')
+        cy.get('input[id="email"]').type('dijana.strbac93+1@gmail.com')
+        cy.get('input[id="password"]').type('dijana123')
+        cy.get('input[id="password-confirmation"]').type('dijana12')
+        cy.get('input[type="checkbox"]').click()
+        cy.get('button[type="submit"]').click()
+    });
+
+
+
+
+
+    it('click register button', () => {
+        cy.visit('/register')
+        cy.get('a[href="/register"]').click()
+    });
+    
+    it('register with first name empty', () => {
+        cy.get('input[id="first-name"]').type('')
+        cy.get('input[id="last-name"]').type('Strbac')
+        cy.get('input[id="email"]').type('dijana.strbac93+1@gmail.com')
+        cy.get('input[id="password"]').type('dijana123')
+        cy.get('input[id="password-confirmation"]').type('dijana123')
+        cy.get('input[type="checkbox"]').click()
+        cy.get('button[type="submit"]').click()
+    });
+
+
+
+
+
+    it('click register button', () => {
+        cy.visit('/register')
+        cy.get('a[href="/register"]').click()
+    });
+    
+    it('register with last name empty', () => {
+        cy.get('input[id="first-name"]').type('Dijana')
+        cy.get('input[id="last-name"]').type('')
+        cy.get('input[id="email"]').type('dijana.strbac93+1@gmail.com')
+        cy.get('input[id="password"]').type('dijana123')
+        cy.get('input[id="password-confirmation"]').type('dijana123')
+        cy.get('input[type="checkbox"]').click()
+        cy.get('button[type="submit"]').click()
+    });
+
+
+
+
+
+
+    it('click register button', () => {
+        cy.visit('/register')
+        cy.get('a[href="/register"]').click()
+    });
+    
+    it('register with all fields with spaces', () => {
+        cy.get('input[id="first-name"]').type('   ')
+        cy.get('input[id="last-name"]').type('   ')
+        cy.get('input[id="email"]').type('   ')
+        cy.get('input[id="password"]').type('   ')
+        cy.get('input[id="password-confirmation"]').type('   ')
+        cy.get('input[type="checkbox"]').click()
+        cy.get('button[type="submit"]').click()
+    });
+
+
+
+
+
+
+    it('click register button', () => {
+        cy.visit('/register')
+        cy.get('a[href="/register"]').click()
+    });
+    
+    it('register with email that alredy exist', () => {
+        cy.get('input[id="first-name"]').type('Dijana')
+        cy.get('input[id="last-name"]').type('Strbac')
+        cy.get('input[id="email"]').type('dijana.strbac93@gmail.com')
+        cy.get('input[id="password"]').type('dijana123')
+        cy.get('input[id="password-confirmation"]').type('dijana123')
+        cy.get('input[type="checkbox"]').click()
+        cy.get('button[type="submit"]').click()
+    });
+
+
+
+
+
+    it('click register button', () => {
+        cy.visit('/register')
+        cy.get('a[href="/register"]').click()
+    });
+    
+    it('register with email with missing .', () => {
+        cy.get('input[id="first-name"]').type('Dijana')
+        cy.get('input[id="last-name"]').type('Strbac')
+        cy.get('input[id="email"]').type('dijana.strbac93@gmailcom')
+        cy.get('input[id="password"]').type('dijana123')
+        cy.get('input[id="password-confirmation"]').type('dijana123')
+        cy.get('input[type="checkbox"]').click()
+        cy.get('button[type="submit"]').click()
+    });
+
+
+
+
+
+
+    it('click register button', () => {
+        cy.visit('/register')
+        cy.get('a[href="/register"]').click()
+    });
+    
+    it('register with space in password', () => {
+        cy.get('input[id="first-name"]').type('Dijana')
+        cy.get('input[id="last-name"]').type('Strbac')
+        cy.get('input[id="email"]').type('dijana.strbac93+1@gmail.com')
+        cy.get('input[id="password"]').type('dijana  123')
+        cy.get('input[id="password-confirmation"]').type('dijana  123')
+        cy.get('input[type="checkbox"]').click()
+        cy.get('button[type="submit"]').click()
+    });
+
+
+
+
+
+
+    it('click register button', () => {
+        cy.visit('/register')
+        cy.get('a[href="/register"]').click()
+    });
+    
+    it('register with all caps on a first name', () => {
+        cy.get('input[id="first-name"]').type('DIJANA')
+        cy.get('input[id="last-name"]').type('Strbac')
+        cy.get('input[id="email"]').type('dijana.strbac93+1@gmail.com')
+        cy.get('input[id="password"]').type('dijana123')
+        cy.get('input[id="password-confirmation"]').type('dijana123')
+        cy.get('input[type="checkbox"]').click()
+        cy.get('button[type="submit"]').click()
+    });
+
+
+
+
+
+   
+
+    it('click register button', () => {
+        cy.visit('/register')
+        cy.get('a[href="/register"]').click()
+    });
+    
+    it('register with password without number', () => {
+        cy.get('input[id="first-name"]').type('DIJANA')
+        cy.get('input[id="last-name"]').type('Strbac')
+        cy.get('input[id="email"]').type('dijana.strbac93+1@gmail.com')
+        cy.get('input[id="password"]').type('dijanastr')
+        cy.get('input[id="password-confirmation"]').type('dijanastr')
+        cy.get('input[type="checkbox"]').click()
+        cy.get('button[type="submit"]').click()
+    });
+
+
+
+
+
+
+    it('click register button', () => {
+        cy.visit('/register')
+        cy.get('a[href="/register"]').click()
+    });
+    
+    it('register with password that contain less then 8 characters', () => {
+        cy.get('input[id="first-name"]').type('DIJANA')
+        cy.get('input[id="last-name"]').type('Strbac')
+        cy.get('input[id="email"]').type('dijana.strbac93+1@gmail.com')
+        cy.get('input[id="password"]').type('dijana1')
+        cy.get('input[id="password-confirmation"]').type('dijana1')
+        cy.get('input[type="checkbox"]').click()
+        cy.get('button[type="submit"]').click()
+    });
+
+
+
+
+
+
+
+    it('click register button', () => {
+        cy.visit('/register')
+        cy.get('a[href="/register"]').click()
+    });
     
     it('register with valid credentials', () => {
         cy.get('input[id="first-name"]').type('Dijana')
@@ -16,5 +272,4 @@ describe('registration case', () => {
         cy.get('input[type="checkbox"]').click()
         cy.get('button[type="submit"]').click()
     });
-   });
-   
+});
